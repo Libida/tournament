a245fc4915f7496dac1a6bd0c5f42a4d5a10b29a
Libida <myr-kat@bk.ru>
2012-04-15 14:35:46 +0300

    Script for new titles

---
+++ install/sql_updates/002.sql
@@ -0,0 +1,7 @@
+UPDATE faqconfig
+SET config_value='Автоматизированная система проведения турниров по шашкам и шахматам'
+WHERE config_name='main.metaDescription';
+
+UPDATE faqconfig
+SET config_value='Турнир'
+WHERE config_name='main.titleFAQ';
